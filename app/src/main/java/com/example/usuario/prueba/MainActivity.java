fa38d00f16bad62d359f6c518f198cbd2ec5d280
caspol <franciscano_01@hotmail.com>
2014-07-18 11:35:40 +0200

    He escrito comentario en MainActivity.java

--- app/src/main/java/com/example/usuario/prueba/MainActivity.java
+++ app/src/main/java/com/example/usuario/prueba/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Activity principal
         setContentView(R.layout.main_activity);
     }
 

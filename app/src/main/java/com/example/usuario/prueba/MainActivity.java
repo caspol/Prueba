8c482755c7a56059346061b8b86c6f4b2ea8fc02
caspol <franciscano_01@hotmail.com>
2014-07-18 11:53:40 +0200

    Update MainActivity.java
    He actualizado el comentario

--- app/src/main/java/com/example/usuario/prueba/MainActivity.java
+++ app/src/main/java/com/example/usuario/prueba/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Activity principal
+        //Activity principal y ahora lo he modificado
         setContentView(R.layout.main_activity);
     }
 
